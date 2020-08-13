418c327ae4ad1407396749fdd4c96d0a06545955
Sanny67 <sayantanichatterjee67@gmail.com>
2020-08-13 15:34:58 +0530

    db setup and admins table 2

--- database/migrations/2020_08_13_100410_create_admins_table.php
+++ database/migrations/2020_08_13_100410_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableAdmins extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateTableAdmins extends Migration
      */
     public function up()
     {
-        Schema::create('table_admins', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('phone');
-            $table->string('email');
-            $table->string('password');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateTableAdmins extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_admins');
+        Schema::dropIfExists('admins');
     }
 }
